659f53766c0f4e046c00f32cf052415d1bbfa679
Grattours <grattours@gmail.com>
2018-12-20 12:58:51 +0100

    Ajouter la fonction Saluer avec la classe "hello"

---
+++ AppliGit/hello.swift
@@ -0,0 +1,14 @@
+//
+//  hello.swift
+//  AppliGit
+//
+//  Created by Luc Derosne on 20/12/2018.
+//  Copyright © 2018 Luc Derosne. All rights reserved.
+//
+
+import Foundation
+class hello {
+    func saluer() {
+         print("hello")
+    }
+}
